fb39889d3c595832cc4fdb48754447ace327de98
Alexandre Freire García <xandrefreire@gmail.com>
2017-11-03 12:22:55 +0100

    Add weather model

---
+++ Core/Common/Domain/Weather.swift
@@ -0,0 +1,19 @@
+//
+//  Weather.swift
+//  Core
+//
+//  Created by Alexandre Freire García on 3/11/17.
+//  Copyright © 2017 Flypay. All rights reserved.
+//
+
+import Foundation
+
+struct Weather {
+    let identifier: Int
+    let name: String
+    let description: String
+    let date: String
+    let city: String
+    let latitude: Double
+    let longitude: Double
+}
